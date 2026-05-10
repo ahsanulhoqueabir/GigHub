52ed8d678b9d92a06ca6b9eb54adcd5ba7878bb5
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2026-05-11 04:16:26 +0600

    feat: update next.config.ts to include image remote patterns for enhanced image handling

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */ allowedDevOrigins: ["192.168.0.5"],
+  allowedDevOrigins: ["192.168.0.5"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "file.briefly60.online",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
